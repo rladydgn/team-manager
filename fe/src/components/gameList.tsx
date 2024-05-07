9f5b62498ef5e88e10226c16092c027012cc7417
dhdgn <dhdgn@naver.com>
2024-05-07 16:33:04 +0900

    fix: 장소 추가

--- fe/src/components/gameList.tsx
+++ fe/src/components/gameList.tsx
@@ -5,6 +5,7 @@ type GameListProps = {
   id: number,
   title: string,
   attendance: number,
+  place: string,
   type: string,
   gameStartAt: string,
   gameEndAt: string,
@@ -29,23 +30,26 @@ export default async function GameList() {
       </Link>
       <table className="min-w-full divide-y divide-gray-800">
         <thead className="bg-theme-color">
-          <tr>
-            <th className="px-6 py-3 text-center text-sm font-bold uppercase">
-              제목
-            </th>
-            <th className="px-6 py-3 text-center text-sm font-bold uppercase">
-              참가자 수
-            </th>
-            <th className="px-6 py-3 text-center text-sm font-bold uppercase">
-              종류
-            </th>
-            <th className="px-6 py-3 text-center text-sm font-bold uppercase">
-              경기 일정
-            </th>
-          </tr>
+        <tr>
+          <th className="px-6 py-3 text-center text-sm font-bold uppercase">
+            제목
+          </th>
+          <th className="px-6 py-3 text-center text-sm font-bold uppercase">
+            참가자 수
+          </th>
+          <th className="px-6 py-3 text-center text-sm font-bold uppercase">
+            종류
+          </th>
+          <th className="px-6 py-3 text-center text-sm font-bold uppercase">
+            장소
+          </th>
+          <th className="px-6 py-3 text-center text-sm font-bold uppercase">
+            경기 일정
+          </th>
+        </tr>
         </thead>
         <tbody className="bg-white divide-y divide-gray-200">
-          {contents.map((item: GameListProps) => (
+        {contents.map((item: GameListProps) => (
             <tr key={item.id} className="hover:bg-gray-100">
               <td className="px-6 py-4 whitespace-nowrap border-r border-gray-300 text-center">
                 {item.title}
@@ -56,14 +60,17 @@ export default async function GameList() {
               <td className="px-6 py-4 whitespace-nowrap border-r border-gray-300 text-center">
                 {item.type}
               </td>
+              <td className="px-6 py-4 whitespace-nowrap border-r border-gray-300 text-center">
+                {item.place}
+              </td>
               <td className="px-6 py-4 whitespace-nowrap text-center">
-                {new Date(item.gameStartAt).toLocaleString('ko-KR')} ~ <br /> {new Date(item.gameEndAt).toLocaleString('ko-KR')}
+                {new Date(item.gameStartAt).toLocaleString('ko-KR')} ~ <br/> {new Date(item.gameEndAt).toLocaleString('ko-KR')}
               </td>
             </tr>
-          ))}
+        ))}
         </tbody>
       </table>
-      <Pagination />
+      <Pagination/>
     </div>
   );
 }
